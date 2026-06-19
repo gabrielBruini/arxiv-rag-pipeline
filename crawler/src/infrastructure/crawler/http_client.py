3eee90a6b933c993de9896c9f4bf528d60382157
Gabriel Bruini <gabriel.souza-bruini@hotmail.com>
2026-06-19 13:39:12 -0300

    chore: remove unitilized code

--- crawler/src/infrastructure/crawler/http_client.py
+++ crawler/src/infrastructure/crawler/http_client.py
@@ -1,14 +1,11 @@
-from pathlib import Path
-
 import requests
 from shared.config import settings
 from shared.logger import get_logger
-from tqdm import tqdm
 
 logger = get_logger(__name__)
 
-class HttpClient:
 
+class HttpClient:
     def get(self, url: str, params: dict | None = None) -> str:
         logger.info(f"GET: {url} | params={params}")
         response = requests.get(url, params=params, timeout=settings.timeout)
